9e41005883dead42f894138dfd9e8e66f3453cbb
crissipires <crissiano_pire@hotmail.com>
2021-09-28 22:51:52 -0300

    :tada: init project

---
+++ src/main/java/com/spring/takeabook/TakeabookApplication.java
@@ -0,0 +1,13 @@
+package com.spring.takeabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeabookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/takeabook/TakeabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.takeabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
